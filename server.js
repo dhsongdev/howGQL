736d9705df0879b0321e954c20493bc59c7498d4
dhsong215 <77286910+dhsongdev@users.noreply.github.com>
2023-03-12 00:06:28 +0900

    resolvers: mutation

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 import { ApolloServer, gql } from 'apollo-server';
-import { tweets } from './db.js';
 
+//db
+let tweets = [
+  {
+    id: '1',
+    text: 'hello world first',
+  },
+  {
+    id: '2',
+    text: 'hello world second',
+  },
+];
+
+//schema define
 const typeDefs = gql`
   type Query {
     allTweets: [Tweet!]!
@@ -21,17 +33,40 @@ const typeDefs = gql`
   }
 `;
 
+//resolvers
 const resolvers = {
   Query: {
     tweet(root, { id }) {
       console.log(id);
       return tweets.find((element) => element.id === id);
     },
+    allTweets() {
+      return tweets;
+    },
+  },
+  Mutation: {
+    postTweet(root, { text, userId }) {
+      const newTweet = {
+        id: tweets.length + 1,
+        text,
+      };
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(root, { id }) {
+      if (!tweets.find((element) => element.id === id)) {
+        return false;
+      }
+      tweets = tweets.filter((item) => !(item.id === id));
+      return true;
+    },
   },
 };
 
+//server define
 const server = new ApolloServer({ typeDefs, resolvers });
 
+//open server
 server.listen().then(({ url }) => {
   console.log(`running on ${url}`);
 });
